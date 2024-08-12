ecac5c530fd41c531ac9e96599e848da0385c3b4
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 17:48:11 +0700

    fix: code🫸🌀✏️📗🐧🐳⬆

--- main.go
+++ main.go
@@ -11,18 +11,6 @@ import (
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
-	}
-
 	const clientID = "<your client id>"
 	const clientSecret = "<your client secret>"
 
